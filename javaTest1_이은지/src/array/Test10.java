c770cddaa002326f9eded35a5ea2da141d035b24
Eunji Lee <bow999@naver.com>
2022-02-06 18:22:23 +0900

    220206 kh 1st commit

--- javaTest1_이은지/src/array/Test10.java
+++ javaTest1_이은지/src/array/Test10.java
@@ -4,22 +4,19 @@ import java.util.ArrayList;
 
 public class Test10 {
 	public static void main(String[] args) {
-		int[][] array = { { 12, 41, 36, 56 },
-						  { 82, 10, 12, 61 },
-						  { 14, 16, 18, 78 },
-						  { 45, 26, 72, 23 } };
+		int[][] array = { { 12, 41, 36, 56 }, { 82, 10, 12, 61 }, { 14, 16, 18, 78 }, { 45, 26, 72, 23 } };
 
 		int[] copyAr = new int[array.length * array[0].length];
 		int num = 0;
 
 		// 1차원 배열에 3의 배수만 기록
-		outer: for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
+		for (int i = 0; i < array.length; i++) {
+			inner: for (int j = 0; j < array[i].length; j++) {
 				if (array[i][j] % 3 == 0) {
 
 					for (int k = 0; k < copyAr.length; k++)
 						if (array[i][j] == copyAr[k])
-							continue outer;
+							continue inner;
 
 					copyAr[num++] = array[i][j];
 				}
