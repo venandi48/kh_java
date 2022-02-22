bed655e7fd7fd9de522414aa1d05603b63787ad2
Eunji Lee <bow999@naver.com>
2022-02-22 09:14:01 +0900

    220222 hw correction

--- 00_Assignment/src/com/exception/charcheck/Run.java
+++ 00_Assignment/src/com/exception/charcheck/Run.java
@@ -14,8 +14,14 @@ public class Run {
 		Scanner sc = new Scanner(System.in);
 		CharacterProcess cp = new CharacterProcess();
 		System.out.print("문자열 입력: ");
-		String s = sc.nextLine();
-		int count = cp.countAlpha(s);
-		System.out.printf("%s에 알파벳은 %d개 있습니다.", s, count);
+		String s = "";
+		try {
+			s = sc.nextLine();
+			int count = cp.countAlpha(s);
+			System.out.printf("%s에 알파벳은 %d개 있습니다.", s, count);
+		} catch (CharCheckException e) {
+			System.err.println(e.getMessage());
+		}
+		
 	}
 }
--- 00_Assignment/src/com/exception/number/Run.java
+++ 00_Assignment/src/com/exception/number/Run.java
@@ -7,20 +7,23 @@ public class Run {
 		Run hw = new Run();
 		hw.test();
 	}
-	
+
 	public void test() {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("정수1 입력: ");
 		int a = sc.nextInt();
 		System.out.print("정수2 입력: ");
 		int b = sc.nextInt();
-		
-		NumberProcess np = new NumberProcess();
-		if(np.checkDouble(a, b)==true)
-			System.out.printf("%d는 %d의 배수이다.", a, b);
-		else
-			System.out.printf("%d는 %d의 배수가 아니다.", a, b);
-			
+		try {
+			NumberProcess np = new NumberProcess();
+			if (np.checkDouble(a, b) == true)
+				System.out.printf("%d는 %d의 배수이다.", a, b);
+			else
+				System.out.printf("%d는 %d의 배수가 아니다.", a, b);
+		} catch (NumberRangeException e) {
+			System.err.println(e.getMessage());
+		}
+
 	}
 }
